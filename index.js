eec477f62b7f303b73a3dd3c84818ba56d7ff046
Taomini <akari.onestime@gmail.com>
2026-09-08 00:05:42 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -27,7 +27,7 @@
     const EXTENSION_SETTINGS_KEY = 'silly-game';
     const DEFAULT_EXTENSION_FOLDER = 'st-game-center';
     const LOADED_SCRIPT_URL = document.currentScript?.src || '';
-    const CURRENT_VERSION = '1.4.9';
+    const CURRENT_VERSION = '1.5.1';
     const UPDATE_CHECK_INTERVAL = 6 * 60 * 60 * 1000;
     const DEFAULT_EXTENSION_SETTINGS = Object.freeze({
         launcherEnabled: true,
